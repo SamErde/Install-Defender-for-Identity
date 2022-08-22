87a192d56371e8ecb50675d48e845de407303a46
Sam Erde <samuel.erde@gmail.com>
2022-08-22 14:18:47 -0400

    Update and rename MDI Network Settings.ps1 to Disable-AllAdaptersLso.ps1
    Renamed, cleaned up the code, added comments, and notes for the script.

--- MDI Network Settings.ps1
+++
@@ -1,7 +0,0 @@
-# Mostly a placeholder for now, but this needs to be set on virtualized domain controllers for MDI
-# This will result in a brief loss of connectivity if/when the change is made
-
-Get-NetAdapterAdvancedProperty | Where-Object DisplayName -Match "^Large*"
-Disable-NetAdapterLso -Name Ethernet0
-
-Get-NetAdapterAdvancedProperty | Where-Object DisplayName -Match "^Large*
