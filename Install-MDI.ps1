b74724ead3ef7a3def979115b9dc35cca300c9a7
Sam Erde <samuel.erde@gmail.com>
2022-08-22 14:24:42 -0400

    Update and rename Install MDI and Remove ATA Sensor.ps1 to Install-MDI.ps1

---
+++ Install-MDI.ps1
@@ -0,0 +1,9 @@
+# WORKING DRAFT - Just don't save your access key here in plain text.
+
+# Install Defender for Identity Sensor (works on Windows Server Core)
+$AccessKey = ''
+.\"Azure ATP sensor Setup.exe" /quiet NetFrameworkCommandLineArguments="/q" AccessKey=$AccessKey
+
+<# Review notes and script the creation of gMSAs and/or service accounts, and give credit: 
+  https://dirteam.com/sander/2022/03/23/howto-programmatically-add-a-microsoft-defender-for-identity-action-account-to-active-directory/
+#>
